2e2a8fce74624db21e6b5e092e9c3988743d7ade
henrique1606 <37597333+henrique1606@users.noreply.github.com>
2024-09-15 11:34:13 -0300

    Descrição das mudanças realizadas

--- app/src/main/java/com/hcc/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/hcc/gastoviagem/MainActivity.kt
@@ -1,17 +1,58 @@
 package com.hcc.gastoviagem
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.hcc.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    // View Binding (Conectar a interface com o código)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
 
-        // View Binding (Conectar a interface com o codigo)
+        // Inicializa o View Binding (Conectar a interface com o código)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCalculate.setOnClickListener(this)
+    }
+
+
+    //Boas praticas: Overide sao as primeiras funcoes
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_calculate) {
+            calculate()
+        }
+    }
+
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != "" &&
+                binding.editPrice.text.toString() != "" &&
+                binding.editAutonomy.text.toString() != "" &&
+                binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculate() {
+
+        if (isValid()){
+            //Toast notification
+            Toast.makeText(this, R.string.success, Toast.LENGTH_LONG).show()
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+            val total = (distance*price)/autonomy
+            binding.textTotalValue.text = "R$ ${"%.2f".format(total)}"
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_LONG).show()
+        }
     }
 }
